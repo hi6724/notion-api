9331fcf452e1d5a2175d62ab3f0ebe7928af2dda
하훈목 <hunmok1027@gmail.com>
2022-10-07 17:22:57 +0900

    createComment

--- src/controller/createCommentById.js
+++ src/controller/createCommentById.js
@@ -1,16 +1,29 @@
 import { Client } from "@notionhq/client";
 
 const notionSecret = "secret_WsQodOj0mpgr6p12JhzFqnbJZxkhJhraJ7WRvyLCofm";
-// const testId = "802466d3-b186-484b-8adc-76431bff20b4";
 
 export const createCommentById = async (req, res) => {
-  //   const notion = new Client({
-  //     auth: notionSecret,
-  //   });
-  //   const result = await notion.comments.create({
-  //     parent: testId,
-  //     rich_text: "Test",
-  //   });
-  const { id, payload, nickname } = req.body;
-  res.send(req.body);
+  const notion = new Client({
+    auth: notionSecret,
+  });
+
+  const {
+    body: { payload, nickname },
+    params: { id },
+  } = req;
+
+  const result = await notion.comments.create({
+    parent: {
+      page_id: id,
+    },
+    rich_text: [
+      {
+        text: {
+          content: nickname + ":" + payload,
+        },
+      },
+    ],
+  });
+
+  res.send({ ok: true, ...result });
 };
