f001557e2c43026430beaeaadc631c1d782fa493
hi6724 <hi6724@gmail.com>
2022-12-13 00:53:11 +0900

    build

--- build/router/router.js
+++ build/router/router.js
@@ -9,6 +9,10 @@ var _express = _interopRequireDefault(require("express"));
 
 var _createCommentById = require("../controller/createCommentById");
 
+var _createGuestBook = require("../controller/guestBookController/createGuestBook");
+
+var _getAllGuestBooks = require("../controller/guestBookController/getAllGuestBooks");
+
 var _getNotionListByCursor = require("../controller/getNotionListByCursor");
 
 var _getPageById = require("../controller/getPageById");
@@ -21,6 +25,10 @@ var _mattermostEmoji = require("../controller/mattermostEmoji");
 
 var _randomShinChan = require("../controller/randomShinChan");
 
+var _deleteGuestBook = require("../controller/guestBookController/deleteGuestBook");
+
+var _editGuestBook = require("../controller/guestBookController/editGuestBook");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var router = _express["default"].Router();
@@ -32,7 +40,12 @@ router.get("/notionList/:cursor", _getNotionListByCursor.getNotionListByCursor);
 router.get("/notion/:id", _getPageById.getPageById);
 router.get("/project/:id", _getProjectById.getProjectById);
 router.get("/projects", _getProjectsList.getProjectList);
-router.post("/notion/:id", _createCommentById.createCommentById);
+router.post("/notion/:id", _createCommentById.createCommentById); // 방명록
+
+router.get("/guestbooks", _getAllGuestBooks.getAllGuestBooks);
+router.post("/guestbooks", _createGuestBook.createGuestBook);
+router.post("/guestbooks/:id", _deleteGuestBook.deleteGuestBook);
+router.put("/guestbooks/:id", _editGuestBook.editGuestBook);
 router.post("/mattermost", _mattermostEmoji.mattermostEmoji);
 router.post("/randomShinChang", _randomShinChan.randomShinChan);
 var _default = router;
