320a004b3c2effa97b3e6ede95ecac81f3a2095f
하훈목 <hunmok1027@gmail.com>
2023-06-10 15:37:41 +0900

    ✨ feat: code일떄 언어 return

--- src/controller/getPageById.js
+++ src/controller/getPageById.js
@@ -1,6 +1,6 @@
-import { Client } from "@notionhq/client";
+import { Client } from '@notionhq/client';
 
-const notionSecret = "secret_WsQodOj0mpgr6p12JhzFqnbJZxkhJhraJ7WRvyLCofm";
+const notionSecret = 'secret_WsQodOj0mpgr6p12JhzFqnbJZxkhJhraJ7WRvyLCofm';
 
 export const getPageById = async ({ params: { id } }, res) => {
   try {
@@ -32,29 +32,42 @@ export const getPageById = async ({ params: { id } }, res) => {
       auth: notionSecret,
     });
 
+    // res.send(blockChild);
+
     const returnArr = blockChild.results
       .map((result) => {
         var type = result.type;
         var payload = result[result.type];
         var link = null;
-        if (type === "divider") {
-          payload = "divider";
+        var language = '';
+        if (type === 'divider') {
+          payload = 'divider';
         }
+
         if (payload.rich_text) {
           if (payload.rich_text.length > 0) {
+            if (type == 'code') {
+              language = payload.language;
+            }
             link = payload.rich_text[0].href;
             // payload = payload.rich_text[0].plain_text;
             payload = payload.rich_text.reduce(
               (acc, value) => acc + value.plain_text,
-              ""
+              ''
             );
           } else {
-            type = "blank";
-            payload = "";
+            type = 'blank';
+            payload = '';
           }
         }
 
-        return { id: result.id, type, payload, link };
+        return {
+          id: result.id,
+          type,
+          payload,
+          link,
+          language,
+        };
       })
       .filter((item) => item !== undefined);
 
